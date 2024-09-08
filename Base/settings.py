9b70446685e146739bc1f450e2aeeaedb8bc9b0b
majinhai <jinhai.ma@icloud.com>
2024-09-08 23:50:59 +0800

    添加跨域支持，使用 django-cors-headers 允许跨域请求

--- Base/settings.py
+++ Base/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -66,8 +68,8 @@ REST_FRAMEWORK = {
     ),
 }
 
-
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True  # 允许所有域名访问
+CORS_ALLOW_CREDENTIALS = True  # 如果你需要传递 Cookie 或凭证
 
 
 SIMPLE_JWT = {
